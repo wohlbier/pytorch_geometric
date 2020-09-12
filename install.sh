f8c0b63ba555b969afef1ce58a49fea5d27ae0a7
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-09-12 12:53:48 -0700

    fix install

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 CUDA=cpu
-
-pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.6.0.html
-pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.6.0.html
-pip install torch-cluster==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.6.0.html
-pip install torch-spline-conv==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-scatter==latest+${CUDA} torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 pip install torch-geometric
